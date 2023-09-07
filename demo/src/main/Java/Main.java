004b9d853940f8e1f02949f0126b4f53b7410991
AleDegia <degia.ale@gmail.com>
2023-09-07 17:30:53 +0200

    yeye

--- demo/src/main/Java/Main.java
+++ demo/src/main/Java/Main.java
@@ -1,9 +1,11 @@
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("ciao");
+        System.out.println("ciaoZio");
         try (Connection conn = DriverManager.getConnection("jdbc:sqlite:orders.db")) {
             for (int i = 1; i <= 10; i++) {
                 // Dati del prodotto
