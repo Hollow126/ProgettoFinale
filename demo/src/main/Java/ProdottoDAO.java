ca5c65ac05f3437847cafffe3a4d24f4d1441ebd
Hollow126 <tizigess@gmail.com>
2023-09-07 16:15:18 +0200

    funziona DAO

---
+++ demo/src/main/Java/ProdottoDAO.java
@@ -0,0 +1,34 @@
+import java.util.List;
+import java.util.ArrayList;
+import java.sql.*;
+
+
+public class ProdottoDAO {
+
+    private Connection conn;
+
+    public ProdottoDAO(Connection conn) {
+        this.conn = conn;
+    }
+
+    public List<Prodotto> getAllProdotti() {
+        List<Prodotto> prodotti = new ArrayList<>();
+
+        try (Statement stmt = conn.createStatement();
+                ResultSet rs = stmt.executeQuery("SELECT * FROM prodotti")) {
+
+            while (rs.next()) {
+                Prodotto p = new Prodotto();
+                p.setId(rs.getInt("id"));
+                p.setNome_Prodotto(rs.getString("nome"));
+                p.setPrezzo(rs.getDouble("prezzo"));
+                prodotti.add(p);
+            }
+        } catch (SQLException e) {
+            // gestisci l'eccezione
+            e.printStackTrace();
+        }
+
+        return prodotti;
+    }
+}
